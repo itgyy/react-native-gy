938ef3cf604a9dc1358d197c354c82e7b71571d0
IT-GYY <851511476@qq.com>
2024-09-07 11:02:29 +0800

    chore: initial commit

---
+++ android/src/main/java/com/gy/GyPackage.kt
@@ -0,0 +1,17 @@
+package com.gy
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class GyPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(GyModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  GyExample
+//
+
+import Foundation
