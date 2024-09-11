d617baf01f5bc67be8217bd1685fad736d737a8c
IT-GYY <851511476@qq.com>
2024-09-11 21:34:47 +0800

    使用其他库

--- android/src/main/java/com/gy/PhotoBrowser/GYPhotoBrowser.kt
+++ android/src/main/java/com/gy/PhotoBrowser/GYPhotoBrowser.kt
@@ -5,12 +5,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
-import com.github.iielse.imageviewer.ImageViewerBuilder
-import com.github.iielse.imageviewer.adapter.ItemType
-import com.github.iielse.imageviewer.core.Photo
-import com.github.iielse.imageviewer.core.SimpleDataProvider
 import com.orhanobut.logger.Logger
-import java.util.HashMap
 
 class GYPhotoBrowser(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -30,31 +25,6 @@ class GYPhotoBrowser(reactContext: ReactApplicationContext) :
   fun show(datas: ReadableArray, index: Int, options: ReadableMap?) {
     Logger.d("显示图片浏览器")
 
-    var photoArray = listOf<GYPhoto>()
-    for (i in 0..datas.size()-1){
-     val item: HashMap<String, Any> = datas.getMap(i).toHashMap();
-      val url:String = item.get("url").toString();
-      val videoUrl:String = item.get("videoUrl").toString();
-      var obj: Any? = null;
-      if(url!= null && url.length != 0){
-        obj = GYPhoto(id = i.toLong(),ItemType.PHOTO,url)
-      }
-      if(videoUrl!= null && videoUrl.length != 0){
-        obj = GYPhoto(id = i.toLong(),ItemType.VIDEO,videoUrl)
-      }
-      Logger.d(obj)
-      if(obj != null){
-        photoArray = photoArray.plus(obj as GYPhoto);
-      }
-    }
-    val builder =  ImageViewerBuilder(
-      context = currentActivity,
-      dataProvider = SimpleDataProvider(photoArray.get(index),photoArray),
-      imageLoader = GYImageLoader(),
-      transformer = GYTransformer()
-    )
-
-    builder.show();
   }
 
 
